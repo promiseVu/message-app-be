202870fd2772c8765757ea0e540daf489014fe43
VU QUOC HUY <vqh8124@gmail.com>
2025-06-08 13:39:55 +0700

    feat: send message

--- src/modules/websocket/events.gateway.ts
+++ src/modules/websocket/events.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
@@ -6,20 +7,63 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { AuthService } from '../auth/auth.service';
+import { MessagesService } from '../messages/messages.service';
+import { CreateMessageDto } from '../messages/dto/create-message.dto';
+import { Socket } from 'socket.io';
 
+interface WebsocketResponse {
+  status: 'success' | 'error';
+  data: any;
+}
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
 export class EventsGateway {
-  constructor(private readonly authService: AuthService) {}
+  private userSocket: Map<string, Set<string>> = new Map();
+  constructor(
+    private readonly authService: AuthService,
+    private readonly messageService: MessagesService,
+  ) {}
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): any {
-    console.log('Received data:', data);
+  @SubscribeMessage('joinConversation')
+  async joinConversation(
+    @MessageBody() payload: { conversationId: string },
+    @ConnectedSocket() socket: Socket,
+  ): Promise<WebsocketResponse> {
+    try {
+      socket.join(payload.conversationId);
+      if (!this.userSocket.has(payload.conversationId)) {
+        this.userSocket.set(payload.conversationId, new Set());
+      }
+      this.userSocket.get(payload.conversationId)?.add(socket.id);
+      const listMessages = await this.messageService.getConversationMessages(
+        payload.conversationId,
+      );
+      return { status: 'success', data: listMessages };
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  @SubscribeMessage('sendMessage')
+  async sendMessage(
+    @MessageBody() payload: { message: CreateMessageDto },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const response = await this.messageService.create({
+      ...payload.message,
+      sender: socket.data.user._id,
+    });
+
+    const message = await this.messageService.findById(response._id.toString());
+
+    this.server
+      .to(payload.message.conversation)
+      .emit('receivedMessage', { status: 'success', data: message });
   }
 
   async handleConnection(socket) {
